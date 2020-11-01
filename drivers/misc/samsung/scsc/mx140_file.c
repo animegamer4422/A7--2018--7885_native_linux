494009fa0b85ef8c6cc8678da8d9c85014c17205
VDavid003 <virag.david003@gmail.com>
2020-11-01 16:22:46 +0100

    Load MX140 WiFi firmware from /lib/firmware/mx140

--- drivers/misc/samsung/scsc/mx140_file.c
+++ drivers/misc/samsung/scsc/mx140_file.c
@@ -21,13 +21,13 @@
 
 #define SCSC_MULTI_RF_CHIP_ID /* Select FW by RF chip ID, not rev */
 
-#if defined(CONFIG_SCSC_CORE_FW_LOCATION) && !defined(CONFIG_SCSC_CORE_FW_LOCATION_AUTO)
+//#if defined(CONFIG_SCSC_CORE_FW_LOCATION) && !defined(CONFIG_SCSC_CORE_FW_LOCATION_AUTO)
 #define MX140_FW_BASE_DIR_SYSTEM_ETC_WIFI      CONFIG_SCSC_CORE_FW_LOCATION
 #define MX140_FW_BASE_DIR_VENDOR_ETC_WIFI      CONFIG_SCSC_CORE_FW_LOCATION
-#else
-#define MX140_FW_BASE_DIR_SYSTEM_ETC_WIFI	"/system/etc/wifi"
-#define MX140_FW_BASE_DIR_VENDOR_ETC_WIFI	"/vendor/etc/wifi"
-#endif
+//#else
+//#define MX140_FW_BASE_DIR_SYSTEM_ETC_WIFI	"/system/etc/wifi"
+//#define MX140_FW_BASE_DIR_VENDOR_ETC_WIFI	"/vendor/etc/wifi"
+//#endif
 
 /* Look for this file in <dir>/etc/wifi */
 #define MX140_FW_DETECT				"mx"
@@ -509,26 +509,6 @@ int mx140_basedir_file(struct scsc_mx *mx)
 	fs = get_fs();
 	/* Set to kernel segment. */
 	set_fs(get_ds());
-
-	/* If /system isn't present, assume platform isn't ready yet */
-	r = vfs_stat("/system", &stat);
-	if (r != 0) {
-		SCSC_TAG_ERR(MX_FILE, "/system not mounted yet\n");
-		r = -EAGAIN;
-		goto done;
-	}
-
-	/* If /vendor isn't present, assume platform isn't ready yet.
-	 * Android M and N still have /vendor, though we don't use it.
-	 */
-	r = vfs_stat("/vendor", &stat);
-	if (r != 0) {
-		SCSC_TAG_ERR(MX_FILE, "/vendor not mounted yet\n");
-		r = -EAGAIN;
-		goto done;
-	}
-
-	/* Now partitions are mounted, so let's see what's in them. */
 
 	/* Try /vendor partition  (Oreo) first.
 	 * If it's present, it'll contain our FW
